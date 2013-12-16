63e5c0b58ec69e5f08c1eb4180714f4199bd4f4b
luca sguanci <luca.sguanci@gmail.com>
2013-12-16 11:26:37 +0100

    versione alto contrasto e icone

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  isWB = 0;
+  $('#wb').bind('click',function(e) {
+    return toggleWB();
+  })
+})
+function toggleWB() {
+  if ( !isWB ) {
+    $('body, div, p, a, h1, h2, h3, h4, h5, h6, strong, em, label').css('background','black').css('color','white');
+    $('#logo').attr('src','img/logo-cap-autolinee-wb.png');
+    isWB = 1;
+    return false;
+  } else {
+    isWB = 0;
+    return true;
+  }
+}
