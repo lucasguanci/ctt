1d5d77d6a7afac579eb092a147c121f7176b04aa
luca sguanci <luca.sguanci@gmail.com>
2013-12-19 12:29:23 +0100

    fixed indentation

--- content-viaggia-con-noi.php
+++ content-viaggia-con-noi.php
@@ -3,39 +3,39 @@
 * Content
 */
 ?>
-<!-- main-content -->
-<div class="col-md-6 col-sm-12 col-md-pull-3 floatright content secondary">
-  <article>
-    <header>
-      <h2>Linee urbane</h2>
-      <ul>
-        <li><i class="fa fa-print"></i>&nbsp;<a href="#">stampa</a></li>
-        <li class="sep">|</li>
-        <li><i class="fa fa-envelope-o"></i>&nbsp;<a href="#">invia a un amico</a></li>
-      </ul>
-    </header>
-    <section class="cnt">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadips   cing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-    </section>
-    <section class="footer">
-      articolo aggiornato il 27.06.2013
-    </section>
-  </article>
-</div>
-<!-- /main-content -->
-<!-- allegati -->
-<div class="col-md-3 col-sm-12 col-md-push-6 floatright allegati">
-  <div class="box">
-    <header>
-      <h2>Documenti allegati</h2>
-    </header>
-    <div class="box-content">
-      <ul>
-        <li><a href="#">Urbano Prato</li>
-        <li><a href="#">Linea V</li>
-        <li><a href="#">Linea F</li>
-      </ul>
-    </div>
-  </div>
-</div>
-<!-- /allegati -->
+          <!-- main-content -->
+          <div class="col-md-6 col-sm-12 col-md-pull-3 floatright content secondary">
+            <article>
+              <header>
+                <h2>Linee urbane</h2>
+                <ul>
+                  <li><i class="fa fa-print"></i>&nbsp;<a href="#">stampa</a></li>
+                  <li class="sep">|</li>
+                  <li><i class="fa fa-envelope-o"></i>&nbsp;<a href="#">invia a un amico</a></li>
+                </ul>
+              </header>
+              <section class="cnt">
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadips   cing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+              </section>
+              <section class="footer">
+                articolo aggiornato il 27.06.2013
+              </section>
+            </article>
+          </div>
+          <!-- /main-content -->
+          <!-- allegati -->
+          <div class="col-md-3 col-sm-12 col-md-push-6 floatright allegati">
+            <div class="box">
+              <header>
+                <h2>Documenti allegati</h2>
+              </header>
+              <div class="box-content">
+                <ul>
+                  <li><a href="#">Urbano Prato</li>
+                  <li><a href="#">Linea V</li>
+                  <li><a href="#">Linea F</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- /allegati -->
--- sidebar.php
+++ sidebar.php
@@ -3,61 +3,61 @@
  * Sidebar
  */
 ?>
-<!-- sidebar -->
-<div class="col-md-3 sidebar">
-  <div class="icons">
-    <ul>
-      <li><a href="#" title="Orari e linee"><i class="fa fa-lg fa-clock-o"></i></a></li>
-      <li><a href="#" title="Biglietti"><i class="fa fa-lg fa-ticket"></i></a></li>
-      <li><a href="#" title="Avvisi"><i class="fa fa-lg fa-wrench"></i></a></li>
-      <li><a href="#" title="Richiesta info"><i class="fa fa-lg fa-rss"></i></a></li>
-      <li><a href="#" title="Twitter"><i class="fa fa-lg fa-twitter"></i></a></li>
-    </ul>
-  </div>
-  <div id="travel-planner" class="box">
-    <header>
-      <p><span class="glyphicon glyphicon-random" style="padding-right: 20px"></span>&nbsp;Travel Planner</p>
-    </header>
-    <div class="box-content">
-      <ul>
-        <li><a href="#" class="active">Percorso</a></li>
-        <li><a href="#">Dove sono</a></li>
-      </ul>
-      <form>
-        <label for="partenza">Partenza</label><br> 
-        <input class="btn btn-default" name="partenza" placeholder="piazza Duomo">
-        <br>
-        <label for="partenza">Destinazione</label><br>
-        <input class="btn btn-default" name="partenza" placeholder="via Marradi 10">
-      </form>
-    </div>
-  </div>
-  <div id="avvisi" class="box">
-    <header>
-      <p><span class="glyphicon glyphicon-list" style="padding-right: 20px"></span>&nbsp;News e avvisi</p>
-    </header>
-    <!-- avvisi e flash news -->
-    <div class="box-content">
-      <p>filtra per <a href="#">avvisi</a> | <a href="#">flash news</a></p>
-      <!-- 1 -->
-      <div class="list-icon">
-        <span class="glyphicon glyphicon-random"></span>
-      </div>
-      <div class="list-content">
-        <h4>12 giugno 2013</h4>
-        <h3>MODIFICA PERCORSO 9</h3>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr...</p> 
-      </div>
-      <!-- 2 -->
-      <div class="list-icon">
-        <span class="glyphicon glyphicon-random"></span>
-      </div>
-      <div class="list-content">
-        <h4>6 ottobre 2013</h4>
-        <h3>navetta gratuita</h3>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr...</p> 
-      </div> 
-    </div>
-  </div>
-</div>
-<!-- /sidebar -->
+          <!-- sidebar -->
+          <div class="col-md-3 sidebar">
+            <div class="icons">
+              <ul>
+                <li><a href="#" title="Orari e linee"><i class="fa fa-lg fa-clock-o"></i></a></li>
+                <li><a href="#" title="Biglietti"><i class="fa fa-lg fa-ticket"></i></a></li>
+                <li><a href="#" title="Avvisi"><i class="fa fa-lg fa-wrench"></i></a></li>
+                <li><a href="#" title="Richiesta info"><i class="fa fa-lg fa-rss"></i></a></li>
+                <li><a href="#" title="Twitter"><i class="fa fa-lg fa-twitter"></i></a></li>
+              </ul>
+            </div>
+            <div id="travel-planner" class="box">
+              <header>
+                <p><span class="glyphicon glyphicon-random" style="padding-right: 20px"></span>&nbsp;Travel Planner</p>
+              </header>
+              <div class="box-content">
+                <ul>
+                  <li><a href="#" class="active">Percorso</a></li>
+                  <li><a href="#">Dove sono</a></li>
+                </ul>
+                <form>
+                  <label for="partenza">Partenza</label><br> 
+                  <input class="btn btn-default" name="partenza" placeholder="piazza Duomo">
+                  <br>
+                  <label for="partenza">Destinazione</label><br>
+                  <input class="btn btn-default" name="partenza" placeholder="via Marradi 10">
+                </form>
+              </div>
+            </div>
+            <div id="avvisi" class="box">
+              <header>
+                <p><span class="glyphicon glyphicon-list" style="padding-right: 20px"></span>&nbsp;News e avvisi</p>
+              </header>
+              <!-- avvisi e flash news -->
+              <div class="box-content">
+                <p>filtra per <a href="#">avvisi</a> | <a href="#">flash news</a></p>
+                <!-- 1 -->
+                <div class="list-icon">
+                  <span class="glyphicon glyphicon-random"></span>
+                </div>
+                <div class="list-content">
+                  <h4>12 giugno 2013</h4>
+                  <h3>MODIFICA PERCORSO 9</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr...</p> 
+                </div>
+                <!-- 2 -->
+                <div class="list-icon">
+                  <span class="glyphicon glyphicon-random"></span>
+                </div>
+                <div class="list-content">
+                  <h4>6 ottobre 2013</h4>
+                  <h3>navetta gratuita</h3>
+                  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr...</p> 
+                </div> 
+              </div>
+            </div>
+          </div>
+          <!-- /sidebar -->
